c0db39c0f6b0e9d36e79a274be832016cf82c327
Michael Stummvoll <michael.stummvoll@staffbase.com>
2023-12-03 15:00:35 +0100

    Cleanup 2015/7

--- src/main/kotlin/org/stummi/aoc/y2015/Day7.kt
+++ src/main/kotlin/org/stummi/aoc/y2015/Day7.kt
@@ -1,120 +1,110 @@
 package org.stummi.aoc.y2015
 
-sealed interface Gatter {
-    fun getVariableInputs(): List<String> = getInputs().filter {
-        isVariable(it)
+import org.stummi.aoc.AdventOfCode
+
+object Day7 : AdventOfCode(2015, 7) {
+
+    data class GateContext(
+        val gatters: Map<String, Gate>,
+        val cache: MutableMap<String, UShort> = mutableMapOf()
+    )
+
+    sealed interface Gate {
+        fun getInputs(): List<String>
+
+        fun getValue(ctx: GateContext): UShort
+
+        fun getValueFromInputs(ctx: GateContext, inputName: String): UShort {
+            return if (inputName[0].isDigit()) {
+                inputName.toUShort()
+            } else if (inputName in ctx.cache) {
+                ctx.cache[inputName]!!
+            } else {
+                val value = ctx.gatters[inputName]?.getValue(ctx)
+                    ?: throw IllegalArgumentException("Unknown input $inputName")
+                ctx.cache[inputName] = value
+                value
+            }
+        }
+
+        val output: String;
     }
 
-    fun isVariable(it: String) = it[0].isLetter()
+    data class NotOp(val input: String, override val output: String) : Gate {
+        override fun getInputs(): List<String> {
+            return listOf(input)
+        }
 
-    fun getInputs(): List<String>
+        override fun getValue(ctx: GateContext): UShort {
+            return getValueFromInputs(ctx, this.input).inv()
+        }
 
-    fun getValue(resolver: (String) -> UShort): UShort
 
-    fun getValueFromInputs(resolver: (String) -> UShort, inputName: String): UShort {
-        return if (inputName[0].isDigit()) {
-            inputName.toUShort()
-        } else {
-            resolver(inputName)
+    }
+
+    data class StaticOp(val input: String, override val output: String) : Gate {
+        override fun getInputs(): List<String> {
+            return listOf(input)
+        }
+
+        override fun getValue(ctx: GateContext): UShort {
+            return getValueFromInputs(ctx, this.input)
         }
     }
 
-    val output: String;
-}
+    data class BinOp(val leftInput: String, val op: String, val rightInput: String, override val output: String) : Gate {
+        override fun getInputs(): List<String> {
+            return listOf(leftInput, rightInput)
+        }
 
-data class NotOp(val input: String, override val output: String) : Gatter {
-    override fun getInputs(): List<String> {
-        return listOf(input)
-    }
+        override fun getValue(ctx: GateContext): UShort {
+            val left = getValueFromInputs(ctx, leftInput)
+            val right = getValueFromInputs(ctx, rightInput)
 
-    override fun getValue(resolver: (String) -> UShort): UShort {
-        return getValueFromInputs(resolver, this.input).inv()
-    }
-
-
-}
-
-data class StaticOp(val input: String, override val output: String) : Gatter {
-    override fun getInputs(): List<String> {
-        return listOf(input)
-    }
-
-    override fun getValue(resolver: (String) -> UShort): UShort {
-        return getValueFromInputs(resolver, this.input)
-    }
-}
-
-data class BinOp(val leftInput: String, val op: String, val rightInput: String, override val output: String) : Gatter {
-    override fun getInputs(): List<String> {
-        return listOf(leftInput, rightInput)
-    }
-
-    override fun getValue(resolver: (String) -> UShort): UShort {
-        val left = getValueFromInputs(resolver, leftInput)
-        val right = getValueFromInputs(resolver, rightInput)
-
-        return when (op) {
-            "LSHIFT" -> (left.toInt() shl right.toInt()).toUShort()
-            "RSHIFT" -> (left.toInt() shr right.toInt()).toUShort()
-            "OR" -> (left.toInt() or right.toInt()).toUShort()
-            "AND" -> (left.toInt() and right.toInt()).toUShort()
-            else -> throw NotImplementedError("$left $op $right")
+            return when (op) {
+                "LSHIFT" -> (left.toInt() shl right.toInt()).toUShort()
+                "RSHIFT" -> (left.toInt() shr right.toInt()).toUShort()
+                "OR" -> (left.toInt() or right.toInt()).toUShort()
+                "AND" -> (left.toInt() and right.toInt()).toUShort()
+                else -> throw NotImplementedError("$left $op $right")
+            }
         }
     }
-}
 
-fun main() {
-    val input = Unit.javaClass.getResourceAsStream("/2015/7.txt").use {
-        it!!.bufferedReader().readLines()
-    }.map {
-        it.split(" -> ").let { s ->
+    private fun parseInput() = input().map { line ->
+        line.split(" -> ").let { s ->
             s[0].split(" ") to s[1]
         }
-    }.map {
-        when (it.first.size) {
-            1 -> StaticOp(it.first[0], it.second)
-            2 -> NotOp(it.first[1], it.second)
-            3 -> BinOp(it.first[0], it.first[1], it.first[2], it.second)
+    }.map { (cmd, output) ->
+        when (cmd.size) {
+            1 -> StaticOp(cmd[0], output)
+            2 -> NotOp(cmd[1], output)
+            3 -> BinOp(cmd[0], cmd[1], cmd[2], output)
             else -> throw IllegalArgumentException()
         }
-    }.toList()
+    }.associateBy {
+        it.output
+    }
 
-    //val aOut = input.filter { it.output == "a" }.first()
-    //println(aOut.getValue(input))
-
-
-    var remaining = input;
-    var currentList = emptyList<Gatter>()
-
-    while (remaining.isNotEmpty()) {
-        val knownInputs = currentList.map { it.output }.toList()
-        val p = remaining.partition {
-            it.getVariableInputs().all { it in knownInputs }
+    override val part1: Any
+        get() {
+            val gatters = parseInput()
+            return gatters["a"]!!.getValue(GateContext(gatters))
         }
 
-        if (p.first.isEmpty()) {
-            throw IllegalArgumentException("something is wrong")
-
+    override val part2: Any
+        get() {
+            val gatters = parseInput()
+            val ctx = GateContext(gatters)
+            val a = gatters["a"]!!.getValue(ctx)
+            ctx.cache.clear()
+            ctx.cache["b"] = a
+            return gatters["a"]!!.getValue(ctx)
         }
-        currentList = currentList + p.first
-        remaining = p.second
+}
 
 
-    }
-
-
-    val map = mutableMapOf<String, UShort>()
-    val resolver: (String) -> UShort = { s -> map[s]!! }
-    currentList.forEach {
-        val value = it.getValue(resolver)
-        println("$value // $it")
-        map[it.output] = value
-    }
-
-    map.toSortedMap().forEach { k, v ->
-        println("$k $v")
-    }
-
-
+fun main() {
+    Day7.fancyRun()
 }
 
