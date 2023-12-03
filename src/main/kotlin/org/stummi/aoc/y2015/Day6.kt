43e88365866933b868f7d0527f29cad6acd4e027
Michael Stummvoll <michael.stummvoll@staffbase.com>
2023-12-03 12:57:03 +0100

    Reactor 2015/6

--- src/main/kotlin/org/stummi/aoc/y2015/Day6.kt
+++ src/main/kotlin/org/stummi/aoc/y2015/Day6.kt
@@ -1,48 +1,76 @@
 package org.stummi.aoc.y2015
 
+import org.stummi.aoc.AdventOfCode
+import org.stummi.aoc.helper.XY
+import org.stummi.aoc.helper.XYRange
 import kotlin.math.max
 
-data class LightCmd(val cmd: String, val start: Pair<Int, Int>, val end: Pair<Int, Int>)
-
-fun main() {
-    val matrix = IntArray(1000 * 1000)
-
-    Unit.javaClass.getResourceAsStream("/2015/6.txt").use {
-        it!!.bufferedReader().readLines()
-    }.map {
-        it.split(" ").let {
-            if (it.size == 4) {
-                it
-            } else {
-                it.drop(1)
-            }
+object Day6 : AdventOfCode(2015, 6) {
+    class LightMatrix(
+        private val matrix: IntArray = IntArray(1000 * 1000)
+    ) {
+        operator fun get(xy: XY) = matrix[posToIdx(xy)]
+        operator fun set(xy: XY, value: Int) {
+            matrix[posToIdx(xy)] = value
         }
-    }.map {
-        LightCmd(it[0], parsePoint(it[1]), parsePoint(it[3]))
-    }.forEach {
-        val sx = it.start.first
-        val ex = it.end.first
-        val sy = it.start.second
-        val ey = it.end.second
-        (sx..ex).forEach { x ->
-            (sy..ey).forEach { y ->
-                val pos = x * 1000 + y
-                // println(pos)
-                when (it.cmd) {
-                    "on" -> matrix[pos]++
-                    "off" -> matrix[pos] = max(matrix[pos] - 1, 0)
-                    "toggle" -> matrix[pos] += 2
+
+        private fun posToIdx(xy: XY) = xy.x * 1000 + xy.y
+
+        fun sum() = matrix.sum()
+    }
+
+    data class LightCmd(val cmd: String, val start: XY, val end: XY) {
+        fun apply(func: (XY) -> Unit) {
+            XYRange(start, end).asSequence().forEach(func)
+        }
+    }
+
+    override val part1: Int
+        get() {
+            val matrix = LightMatrix()
+            parseInput().forEach { cmd ->
+                when(cmd.cmd) {
+                    "on" -> cmd.apply { matrix[it] = 1 }
+                    "off" -> cmd.apply { matrix[it] = 0 }
+                    "toggle" -> cmd.apply { matrix[it] = 1 - matrix[it] }
                     else -> throw IllegalArgumentException()
                 }
             }
+            return matrix.sum()
         }
-        //  println(it)
 
+    override val part2: Int
+        get() {
+            val matrix = LightMatrix()
+            parseInput().forEach { cmd ->
+                when(cmd.cmd) {
+                    "on" -> cmd.apply { matrix[it]++ }
+                    "off" -> cmd.apply { matrix[it] = (matrix[it] - 1).coerceAtLeast(0) }
+                    "toggle" -> cmd.apply { matrix[it] += 2 }
+                    else -> throw IllegalArgumentException()
+                }
+            }
+            return matrix.sum()
+        }
+
+    private fun parseInput() =
+        input().map {
+            it.split(" ").let { spl ->
+                if (spl.size == 4) {
+                    spl
+                } else {
+                    spl.drop(1)
+                }
+            }
+        }.map {
+            Day6.LightCmd(it[0], parsePoint(it[1]), parsePoint(it[3]))
+        }
+
+    fun parsePoint(s: String) = s.split(",").let {
+        XY(it[0].toInt(), it[1].toInt())
     }
-
-    println(matrix.sum())
 }
 
-fun parsePoint(s: String) = s.split(",").let {
-    it[0].toInt() to it[1].toInt()
+fun main() {
+    Day6.fancyRun()
 }
