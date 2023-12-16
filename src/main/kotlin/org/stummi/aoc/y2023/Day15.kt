2cc1b9c114bb8513abab950dc4789e4dcfc230f5
Michael Stummvoll <michael.stummvoll@staffbase.com>
2023-12-16 12:59:33 +0100

    Add 2023/15..16

--- src/main/kotlin/org/stummi/aoc/y2023/Day15.kt
+++ src/main/kotlin/org/stummi/aoc/y2023/Day15.kt
@@ -3,6 +3,61 @@ package org.stummi.aoc.y2023
 import org.stummi.aoc.AdventOfCode
 
 object Day15 : AdventOfCode(2023, 15) {
+
+    init {
+        rawSample("HASH", 52L, Unit)
+        rawSample("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7", 1320L)
+    }
+
+    private fun hash(s: String): Int {
+        var hash = 0
+        s.chars().forEach {
+            hash += it
+            hash *= 17
+            hash %= 256
+        }
+        return hash
+    }
+
+    override val part1: Any
+        get() =
+            input.lines.first().split(",").sumOf {
+                hash(it).toLong()
+            }
+
+    override val part2: Any
+        get() {
+
+            val boxes = generateSequence { mutableListOf<Pair<String, Int>>() }.take(256).toList()
+
+            input.lines.first().split(",").forEach {
+                if (it.endsWith("-")) {
+                    val label = it.dropLast(1)
+                    val hash = hash(label)
+                    boxes[hash].removeAll { (l, _) -> l == label }
+                } else if ("=" in it) {
+                    val spl = it.split("=")
+                    val label = spl[0]
+                    val v = spl[1].toInt()
+                    val hash = hash(label)
+                    val index = boxes[hash].indexOfFirst { (l, _) -> l == label }
+                    if (index >= 0) {
+                        boxes[hash][index] = label to v
+                    } else {
+                        boxes[hash].add(label to v)
+                    }
+                } else {
+                    TODO()
+                }
+            }
+
+
+            return boxes.withIndex().sumOf { (boxIdx, box) ->
+                box.withIndex().sumOf { (slot, p) ->
+                    (boxIdx + 1) * (slot + 1) * p.second
+                }
+            }
+        }
 }
 
 fun main() {
--- src/main/kotlin/org/stummi/aoc/y2023/Day16.kt
+++ src/main/kotlin/org/stummi/aoc/y2023/Day16.kt
@@ -1,8 +1,141 @@
 package org.stummi.aoc.y2023
 
 import org.stummi.aoc.AdventOfCode
+import org.stummi.aoc.helper.BoolMatrix
+import org.stummi.aoc.helper.CharMatrix
+import org.stummi.aoc.helper.XY
 
 object Day16 : AdventOfCode(2023, 16) {
+
+    enum class Direction(val func: (XY) -> XY) {
+        UP(XY::up),
+        LEFT(XY::left),
+        DOWN(XY::down),
+        RIGHT(XY::right),
+    }
+
+    val transformations = mapOf<Char, (Direction) -> List<Direction>>(
+        '|' to {
+            if (it in listOf(Direction.UP, Direction.DOWN)) {
+                listOf(it)
+            } else {
+                listOf(Direction.UP, Direction.DOWN)
+            }
+        },
+        '-' to {
+            if (it in listOf(Direction.LEFT, Direction.RIGHT)) {
+                listOf(it)
+            } else {
+                listOf(Direction.LEFT, Direction.RIGHT)
+            }
+        },
+        '/' to {
+            listOf(
+                when (it) {
+                    Direction.LEFT -> Direction.DOWN
+                    Direction.DOWN -> Direction.LEFT
+                    Direction.RIGHT -> Direction.UP
+                    Direction.UP -> Direction.RIGHT
+                }
+            )
+        },
+        '\\' to {
+            listOf(
+                when (it) {
+                    Direction.LEFT -> Direction.UP
+                    Direction.DOWN -> Direction.RIGHT
+                    Direction.RIGHT -> Direction.DOWN
+                    Direction.UP -> Direction.LEFT
+                }
+            )
+        }
+    )
+
+    private fun simulate(map: CharMatrix, startPos: XY = XY.ZERO, startDirection: Direction = Direction.RIGHT): Int {
+        val rayMap = BoolMatrix(map.bounds)
+
+        val openRays = mutableListOf(
+            startPos to startDirection
+        )
+
+        val closedRays = mutableSetOf<Pair<XY, Direction>>()
+        while (openRays.isNotEmpty()) {
+            val ray = openRays.removeFirst()
+            var (pos, direction) = ray
+            closedRays.add(ray)
+
+            while (true) {
+                rayMap[pos] = true
+                if (map[pos] in transformations) {
+                    transformations[map[pos]]!!(direction).forEach {
+                        val newRay = it.func(pos) to it
+                        if (newRay !in closedRays && newRay.first in map.bounds) {
+                            openRays.add(newRay)
+                        }
+                    }
+                    break
+                }
+
+                pos = direction.func(pos)
+
+                if (pos !in map.bounds) {
+                    break
+                }
+
+            }
+        }
+
+        /*
+            map.bounds.printAsMap {
+                if (rayMap[it]) {
+                    '#'
+                } else {
+                    map[it]
+                }
+            }
+
+             */
+        return rayMap.values.cardinality()
+    }
+
+
+    override val part1: Any
+        get() {
+            val map = input.matrix
+            return simulate(map, XY.ZERO, Direction.RIGHT)
+        }
+
+
+    override val part2: Any
+        get() {
+            val map = input.matrix
+            return sequence {
+                yield(map.bounds.topLeft to Direction.DOWN)
+                yield(map.bounds.topLeft to Direction.RIGHT)
+
+                yield(map.bounds.bottomLeft to Direction.UP)
+                yield(map.bounds.bottomLeft to Direction.RIGHT)
+
+                yield(map.bounds.topRight to Direction.DOWN)
+                yield(map.bounds.topRight to Direction.LEFT)
+
+                yield(map.bounds.bottomLeft to Direction.UP)
+                yield(map.bounds.bottomRight to Direction.LEFT)
+
+                (1..<map.bounds.right).forEach {
+                    yield(XY(it, 0) to Direction.DOWN)
+                    yield(XY(it, map.bounds.bottom) to Direction.UP)
+                }
+
+                (1..<map.bounds.bottom).forEach {
+                    yield(XY(0, it) to Direction.RIGHT)
+                    yield(XY(map.bounds.right, it) to Direction.LEFT)
+                }
+            }.maxOf { (pos, dir) ->
+                simulate(map, pos, dir)
+            }
+        }
+
 }
 
 fun main() {
