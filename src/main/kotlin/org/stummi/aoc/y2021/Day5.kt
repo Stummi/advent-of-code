c7c59ecf0eb1ef1f2d5e434b61970e7bcede8611
Michael Stummvoll <michael.stummvoll@staffbase.com>
2023-12-17 16:09:14 +0100

    Refactor 2021/5

--- src/main/kotlin/org/stummi/aoc/y2021/Day5.kt
+++ src/main/kotlin/org/stummi/aoc/y2021/Day5.kt
@@ -1,95 +1,84 @@
-import java.io.File
-import java.lang.Integer.max
-import java.lang.Integer.min
+import org.stummi.aoc.AdventOfCode
+import org.stummi.aoc.helper.IntMatrix
+import org.stummi.aoc.helper.XY
 import kotlin.math.abs
 
-data class Point(val x: Int, val y: Int) {
-    override fun toString(): String = "$x,$y"
-}
+object Day5 : AdventOfCode(2021, 5) {
+    data class Line(val start: XY, val end: XY) {
+        fun isStraightLine() =
+            start.x == end.x || start.y == end.y
 
-data class Line(val start: Point, val end: Point) {
-    fun isStraightLine() =
-        start.x == end.x || start.y == end.y
+        fun isHorizontal() =
+            start.y == end.y
 
-    fun isHorizontal() =
-        start.y == end.y
+        fun isVertical() =
+            start.x == end.x
 
-    fun isVertical() =
-        start.x == end.x
+        fun isDiagonal(): Boolean {
+            return abs(start.y - end.y) == abs(start.x - end.x)
+        }
 
-    fun isDiagonal(): Boolean {
-        return abs(start.y - end.y) == abs(start.x - end.x)
+        override fun toString() = "$start -> $end"
     }
 
-    override fun toString() = "$start -> $end"
-
-}
-
-data class Matrix(
-    val w: Int,
-    val h: Int,
-) {
-    val data = IntArray(w * h)
-
-    fun translatePos(x: Int, y: Int) = x * w + y
-
-    operator fun get(x: Int, y: Int) =
-        data[translatePos(x, y)]
-
-
-    operator fun set(x: Int, y: Int, v: Int) {
-        data[translatePos(x, y)] = v
+    private val parsedInput by lazy {
+        input.lines.map {
+            it.split(" -> ").map {
+                it.split(",").let {
+                    XY(it[0].toInt(), it[1].toInt())
+                }
+            }.let {
+                Line(it[0], it[1])
+            }
+        }
     }
+
+    fun solve(considerDiagonals: Boolean): Int {
+        val m = IntMatrix(XY.ZERO..XY(1000,1000))
+
+        parsedInput.forEach { l ->
+            if (l.isHorizontal()) {
+                rangeHelper(l.start.x, l.end.x).forEach {
+                    m[XY(it, l.start.y)]++; // = m[it, l.start.y] + 1
+                }
+            } else if (l.isVertical()) {
+                rangeHelper(l.start.y, l.end.y).forEach {
+                    m[XY(l.start.x, it)]++; // = m[it, l.start.y] + 1
+                }
+            } else if (considerDiagonals) {
+                if (l.start.x - l.end.x == l.start.y - l.end.y) {
+                    val sx = minOf(l.start.x, l.end.x)
+                    val sy = minOf(l.start.y, l.end.y)
+                    val steps = abs(l.start.x - l.end.x)
+                    (0..steps).forEach {
+                        m[XY(sx + it, sy + it)]++
+                    }
+                } else {
+                    val sx = maxOf(l.start.x, l.end.x)
+                    val sy = minOf(l.start.y, l.end.y)
+                    val steps = abs(l.start.x - l.end.x)
+                    (0..steps).forEach {
+                        m[XY(sx - it, sy + it)]++
+                    }
+                }
+            }
+        }
+        return m.allValues.count { it > 1 }
+    }
+
+    fun rangeHelper(a: Int, b: Int) = if (a < b)
+        (a..b)
+    else
+        (b..a)
+
+    override val part1: Any
+        get() = solve(false)
+
+    override val part2: Any
+        get() = solve(true)
 
 }
 
 fun main() {
-    var input = File("/tmp/input.txt").readLines().map {
-        it.split(" -> ").map {
-            it.split(",").let {
-                Point(it[0].toInt(), it[1].toInt());
-            }
-        }.let {
-            Line(it[0], it[1])
-        }
-    }
-
-    println(input.all { it.isStraightLine() || it.isDiagonal() })
-
-    val m = Matrix(1000, 1000)
-
-    input.forEach { l ->
-        if (l.isHorizontal()) {
-            rangeHelper(l.start.x, l.end.x).forEach {
-                m[it, l.start.y]++; // = m[it, l.start.y] + 1
-            }
-        } else if (l.isVertical()) {
-            rangeHelper(l.start.y, l.end.y).forEach {
-                m[l.start.x, it]++; // = m[it, l.start.y] + 1
-            }
-        } else {
-            if (l.start.x - l.end.x == l.start.y - l.end.y) {
-                val sx = min(l.start.x, l.end.x)
-                val sy = min(l.start.y, l.end.y)
-                var steps = abs(l.start.x - l.end.x)
-                (0..steps).forEach {
-                    m[sx + it, sy + it]++
-                }
-            } else {
-                val sx = max(l.start.x, l.end.x)
-                val sy = min(l.start.y, l.end.y)
-                var steps = abs(l.start.x - l.end.x)
-                (0..steps).forEach {
-                    m[sx - it, sy + it]++
-                }
-            }
-        }
-    }
-
-    println(m.data.count { it > 1 })
+    Day5.fancyRun()
 }
-
-fun rangeHelper(a: Int, b: Int) = if (a < b)
-    (a..b)
-else
-    (b..a)
