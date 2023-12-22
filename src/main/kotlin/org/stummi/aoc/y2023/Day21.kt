b8c6e088f2c344ab18cf44f4ed07c734d4aa1d16
Michael Stummvoll <michael.stummvoll@staffbase.com>
2023-12-22 10:45:43 +0100

    Add 2023/21..22

--- src/main/kotlin/org/stummi/aoc/y2023/Day21.kt
+++ src/main/kotlin/org/stummi/aoc/y2023/Day21.kt
@@ -1,8 +1,112 @@
 package org.stummi.aoc.y2023
 
 import org.stummi.aoc.AdventOfCode
+import org.stummi.aoc.helper.CharMatrix
+import org.stummi.aoc.helper.IntMatrix
+import org.stummi.aoc.helper.XY
+import org.stummi.aoc.helper.XYRange
 
-object Day21 : AdventOfCode(2023, 21)
+object Day21 : AdventOfCode(2023, 21) {
+
+    init {
+        resourceSample("demo", additionalData = 6)
+    }
+
+    private val map by lazy { input.matrix }
+    override val part1: Any
+        get() {
+            val startPos = map.find('S')
+            return getPossiblePlots(map, startPos, 64.orSample()).count()
+        }
+
+    override val part2: Any
+        get() {
+            TODO()
+        }
+
+    private fun floodFillMap(map: CharMatrix, startPosition: XY): IntMatrix {
+        val returnValue = IntMatrix(map.bounds, IntArray(map.bounds.area) { -1 })
+        returnValue[startPosition] = 0
+        val openList = mutableListOf(startPosition)
+        while (openList.isNotEmpty()) {
+            val pos = openList.removeFirst()
+            val cost = returnValue[pos]
+            pos.orthogonalNeighbours().forEach {
+                if (it in map.bounds && map[it] != '#' && returnValue[it] == -1) {
+                    returnValue[it] = cost + 1
+                    openList.add(it)
+                }
+            }
+        }
+        return returnValue
+    }
+
+    private fun getPossiblePlots(
+        map: CharMatrix,
+        position: XY,
+        steps: Int,
+        cache: MutableMap<Pair<XY, Int>, Set<XY>> = mutableMapOf()
+    ): Set<XY> {
+        if (steps == 0) {
+            return setOf(position)
+        }
+
+        cache[position to steps]?.let { return it }
+
+
+        val t = translatePoint(position, map.bounds)
+        if (t != position) {
+            val diff = t - position
+            return getPossiblePlots(map, t, steps, cache).asSequence().map {
+                it - diff
+            }.toSet().also { ret ->
+                cache[position to steps] = ret
+            }
+        }
+
+
+        val tmp = mutableSetOf<XY>()
+        position.orthogonalNeighbours().filterNot { map[translatePoint(it, map.bounds)] == '#' }.map {
+            getPossiblePlots(map, it, steps - 1, cache)
+        }.forEach { tmp.addAll(it) }
+
+        val ret = tmp.toSet()
+        cache[position to steps] = ret
+        return ret
+    }
+
+    private fun translatePoint(position: XY, bounds: XYRange): XY {
+        if (position in bounds) {
+            return position
+        } else {
+            return XY(
+                translateIntoRange(position.x, bounds.xRange),
+                translateIntoRange(position.y, bounds.yRange)
+            )
+        }
+    }
+
+
+    fun translateIntoRange(v: Int, r: IntRange): Int {
+        check(!r.isEmpty())
+
+        if (v in r) {
+            return v
+        }
+
+        val rangeLen = r.last - r.first + 1
+
+        if (v > r.last) {
+            return r.first + (v - r.first) % rangeLen
+        }
+
+        if (v < r.first) {
+            return r.last - (r.first - v - 1) % rangeLen
+        }
+
+        TODO()
+    }
+}
 
 fun main() {
     Day21.fancyRun()
--- src/main/kotlin/org/stummi/aoc/y2023/Day22.kt
+++ src/main/kotlin/org/stummi/aoc/y2023/Day22.kt
@@ -2,7 +2,129 @@ package org.stummi.aoc.y2023
 
 import org.stummi.aoc.AdventOfCode
 
-object Day22 : AdventOfCode(2023, 22)
+object Day22 : AdventOfCode(2023, 22) {
+    private data class XYZ(
+        val x: Int,
+        val y: Int,
+        val z: Int,
+    ) {
+        val down: XYZ
+            get() {
+                check(z > 1)
+                return XYZ(x, y, z - 1)
+            }
+
+        override fun toString(): String = "($x,$y,$z)"
+    }
+
+    private data class Brick(
+        val start: XYZ,
+        val end: XYZ,
+        val id: Int,
+    ) {
+        val cubes: List<XYZ>
+            get() {
+                return when {
+                    start.x != end.x -> (start.x..end.x).map { XYZ(it, start.y, start.z) }
+                    start.y != end.y -> (start.y..end.y).map { XYZ(start.x, it, start.z) }
+                    start.z != end.z -> (start.z..end.z).map { XYZ(start.x, start.y, it) }
+                    else -> listOf(start)
+                }
+            }
+
+        val down: Brick get() = Brick(start.down, end.down, id)
+
+        override fun toString(): String = "$start~$end"
+    }
+
+    private class Field(
+        ctorBricks: List<Brick>,
+    ) {
+        val bricks = ctorBricks.toMutableList()
+        val occupiedPoints = ctorBricks.flatMap { it.cubes }.toMutableSet()
+
+        fun partsMovableDown(): List<Brick> {
+            return bricks.filter { brick ->
+                brick.start.z > 1 &&
+
+                        brick.down.cubes.none {
+                            it !in brick.cubes &&
+                                    it in occupiedPoints
+                        }
+            }
+        }
+
+        fun moveDown(b: Brick) {
+            check(bricks.remove(b))
+            b.cubes.forEach {
+                check(occupiedPoints.remove(it))
+            }
+            val down = b.down
+            bricks.add(down)
+            down.cubes.forEach {
+                check(occupiedPoints.add(it))
+            }
+
+        }
+    }
+
+    private val parsedInput by lazy {
+        input.lines.mapIndexed { idx, line ->
+            line.split("~").map {
+                it.split(",").map(String::toInt).let { (x, y, z) -> XYZ(x, y, z) }
+            }.let { (start, end) ->
+                Brick(start, end, idx)
+            }
+        }
+    }
+
+    override val part1: Any
+        get() {
+            val f = Field(parsedInput)
+            settleField(f)
+
+            return f.bricks.count {
+                Field(f.bricks - it).partsMovableDown().isEmpty()
+            }
+        }
+
+    override val part2: Any
+        get() {
+            val f = Field(parsedInput)
+            settleField(f)
+
+            val bricksById = f.bricks.associateBy { it.id }
+
+            return f.bricks.sumOf {
+                val newField = Field(f.bricks - it)
+                if (!settleField(newField)) {
+                    return@sumOf 0
+                }
+
+                newField.bricks.count {
+                    it != bricksById[it.id]
+                }
+            }
+        }
+
+    private fun settleField(f: Field): Boolean {
+        var changed = false
+        while (true) {
+            val down = f.partsMovableDown()
+            if (down.isEmpty()) {
+                break
+            }
+            changed = true
+
+            down.forEach {
+                f.moveDown(it)
+            }
+        }
+        return changed
+    }
+
+
+}
 
 fun main() {
     Day22.fancyRun()
